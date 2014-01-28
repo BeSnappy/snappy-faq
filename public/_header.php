a518f62ea9604ed91cecee927f14782bcb0718b6
Eric Barnes <eric@ericlbarnes.com>
2014-01-28 16:58:58 -0500

    Adjust the header styles.

--- public/_header.php
+++ public/_header.php
@@ -9,7 +9,7 @@
 	<link href="themes/default/style.css" rel="stylesheet">
 	<!-- <link href="themes/customers/slideroom.css" rel="stylesheet"> -->
 	<!-- <link href="themes/freelancer/style.css" rel="stylesheet"> -->
-	<!-- <link href="themes/besnappy/snappyfaq.css" rel="stylesheet"> -->
+<!--	 <link href="themes/besnappy/snappyfaq.css" rel="stylesheet">-->
 	<script src="vendor/modernizr/js/modernizr.js"></script>
 </head>
 <body>
